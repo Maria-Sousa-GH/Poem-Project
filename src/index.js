0798f0dae3a6d135a266981e912cfe006074dd70
Maria-Sousa-GH <157738574+Maria-Sousa-GH@users.noreply.github.com>
2024-03-28 19:29:19 +0000

    adjusted the JS to generate the poem automatically based on the keywork in the search bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function createPoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Love Love Love",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function createPoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "facba5oe04e0d0e2497c5fe825bta3c4";
+  let context =
+    "You are a romantic poem expert and you need to create a poem about what love means. It has to contain only 4 paragraphs, and doesn't need to have a Title. It has to be in Portuguese language and in basic HTML.";
+  let prompt = `Create a Portuguese Poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#form-poem");
